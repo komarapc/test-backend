c6da767c33325d32ea2717f3a84a6a4e90e1e011
izmi.komar <komar.izmi@gmail.com>
2024-07-03 01:52:29 +0700

    chore: Refactor BearerMiddleware to handle JWT authentication

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import {
-  NestFastifyApplication,
-  FastifyAdapter,
-} from '@nestjs/platform-fastify';
-import fastifyStatic from '@fastify/static';
-import { join } from 'path';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(
-    AppModule,
-    new FastifyAdapter(),
-  );
-  app.register(fastifyStatic, {
-    root: join(__dirname, '../../public'),
-    prefix: '/public/',
-  });
+  const app = await NestFactory.create(AppModule);
+
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   const config = new DocumentBuilder()

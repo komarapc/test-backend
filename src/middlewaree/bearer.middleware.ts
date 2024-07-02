69d36485a3330d7145791c24dbd49cfab3341768
izmi.komar <komar.izmi@gmail.com>
2024-07-03 01:52:19 +0700

    chore: Refactor BearerMiddleware to handle JWT authentication

--- src/middlewaree/bearer.middleware.ts
+++ src/middlewaree/bearer.middleware.ts
@@ -1,11 +1,37 @@
-import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
+import {
+  ForbiddenException,
+  HttpCode,
+  HttpStatus,
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+  MethodNotAllowedException,
+  NestMiddleware,
+  Req,
+  RequestMethod,
+  Res,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { NextFunction, Request, Response } from 'express';
-
+import * as jwt from 'jsonwebtoken';
 @Injectable()
 export class BearerMiddleware implements NestMiddleware {
   private readonly logger = new Logger(BearerMiddleware.name);
   async use(req: Request, res: Response, next: NextFunction) {
-    this.logger.log(BearerMiddleware.name);
+    const authorization = req.headers.authorization;
+    if (!authorization) throw new UnauthorizedException();
+    const [bearer, token] = authorization.split(' ');
+    if (bearer !== 'Bearer' || !token) throw new UnauthorizedException();
+    try {
+      const payload: any = jwt.verify(token, process.env.JWT_SECRET);
+      if (!Object.keys(payload).length) throw new UnauthorizedException();
+      if (!payload.is_admin && req.method !== 'GET')
+        throw new ForbiddenException();
+    } catch (error) {
+      this.logger.error(error);
+      res.status(error.status).send(error.response);
+      return;
+    }
     next();
   }
 }
